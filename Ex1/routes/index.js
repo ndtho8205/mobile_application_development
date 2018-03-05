ad14cbd931a11fbb6cd780a9f7072619457a861c
Tho Nguyen Duc <ndtho8205@gmail.com>
2018-03-05 14:36:41 +0700

    Render index.html

--- Ex1/routes/index.js
+++ Ex1/routes/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const path = require('path');
 
 const router = express.Router();
 
 /* GET homepage */
 router.get('/', (req, res) => {
-  res.send('index');
+  res.sendFile(path.join(`${__dirname}/../public/index.html`));
 });
 
 module.exports = router;
