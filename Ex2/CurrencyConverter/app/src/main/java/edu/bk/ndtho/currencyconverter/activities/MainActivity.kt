2486375d6e7808ca1b4d9c07afc2cab0acfbfe13
Tho Nguyen Duc <ndtho8205@gmail.com>
2018-04-17 11:04:10 +0700

    Change currency selection dialog

--- Ex2/CurrencyConverter/app/src/main/java/edu/bk/ndtho/currencyconverter/activities/MainActivity.kt
+++ Ex2/CurrencyConverter/app/src/main/java/edu/bk/ndtho/currencyconverter/activities/MainActivity.kt
@@ -5,7 +5,6 @@ import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.text.Editable
 import android.text.TextWatcher
-import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import edu.bk.ndtho.currencyconverter.R
@@ -13,12 +12,15 @@ import edu.bk.ndtho.currencyconverter.models.Currency
 import edu.bk.ndtho.currencyconverter.models.CurrencyData
 import edu.bk.ndtho.currencyconverter.utils.CurrencyDataHandler
 import kotlinx.android.synthetic.main.activity_main.*
+import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity() {
 
     private val mCurrencyDataHandler = CurrencyDataHandler()
     private lateinit var mCurrencyData: CurrencyData
 
+    private var mBaseCurrencyIndex: Int = 0
+    private var mQuoteCurrencyIndex: Int = 1
     private lateinit var mBaseCurrency: Currency
     private lateinit var mQuoteCurrency: Currency
 
@@ -28,23 +30,26 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         mCurrencyData = mCurrencyDataHandler.loadFromJson(this, R.raw.currencies)
-        mBaseCurrency = mCurrencyData.currencies[0]
-        mQuoteCurrency = mCurrencyData.currencies[1]
+        mBaseCurrency = mCurrencyData.currencies[mBaseCurrencyIndex]
+        mQuoteCurrency = mCurrencyData.currencies[mQuoteCurrencyIndex]
 
         initViews()
     }
 
     private fun initViews() {
         updateViewsWhenCurrencyChanged()
+
         initBaseCurrency()
         initQuoteCurrency()
     }
 
     private fun initBaseCurrency() {
-        btnBaseCurrencySelector.showCurrencySelectionDialog {
-            mBaseCurrency = mCurrencyData.currencies[it]
-            updateViewsWhenCurrencyChanged()
-            clearAmount()
+        btnBaseCurrencySelector.setOnClickListener {
+            showCurrencySelectionDialog(mBaseCurrencyIndex, {
+                mBaseCurrencyIndex = it
+                mBaseCurrency = mCurrencyData.currencies[it]
+                updateViewsWhenCurrencyChanged()
+            })
         }
 
         etBaseCurrencyAmount.onMoneyAmountChanged {
@@ -54,10 +59,13 @@ class MainActivity : AppCompatActivity() {
 
 
     private fun initQuoteCurrency() {
-        btnQuoteCurrencySelector.showCurrencySelectionDialog {
-            mQuoteCurrency = mCurrencyData.currencies[it]
-            updateViewsWhenCurrencyChanged()
-            clearAmount()
+        btnQuoteCurrencySelector.setOnClickListener {
+            showCurrencySelectionDialog(mQuoteCurrencyIndex, {
+                mQuoteCurrencyIndex = it
+                mQuoteCurrency = mCurrencyData.currencies[it]
+
+                updateViewsWhenCurrencyChanged()
+            })
         }
 
         etQuoteCurrencyAmount.onMoneyAmountChanged {
@@ -65,38 +73,12 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun clearAmount() {
-        etBaseCurrencyAmount.setText("")
-        etQuoteCurrencyAmount.setText("")
-    }
-
-    private fun updateViewsWhenCurrencyChanged() {
-        btnBaseCurrencySelector.text = mBaseCurrency.name
-        tvBaseCurrencySymbol.text = mBaseCurrency.symbol
-        tvBaseCurrencyUnit.updateCurrencyUnitInfo(mBaseCurrency, mQuoteCurrency)
-        etBaseCurrencyAmount.setText("")
-
-        btnQuoteCurrencySelector.text = mQuoteCurrency.name
-        tvQuoteCurrencySymbol.text = mQuoteCurrency.symbol
-        tvQuoteCurrencyUnit.updateCurrencyUnitInfo(mQuoteCurrency, mBaseCurrency)
-        etQuoteCurrencyAmount.setText("")
-    }
 
     private fun TextView.updateCurrencyUnitInfo(fromCurrency: Currency,
                                                 toCurrency: Currency) {
         this.text = resources.getString(R.string.currencies_unit_info, fromCurrency.code,
                 toCurrency.rate / fromCurrency.rate,
                 toCurrency.code)
-    }
-
-    private fun Button.showCurrencySelectionDialog(onItemSelected: (Int) -> Unit) {
-        this.setOnClickListener {
-            val currencyNameList: Array<String> = mCurrencyData.currencyInfoList.toTypedArray()
-            val builder = AlertDialog.Builder(this@MainActivity)
-            builder.setTitle("Select currency")
-            builder.setItems(currencyNameList, { _, which -> onItemSelected(which) })
-            builder.show()
-        }
     }
 
     private fun EditText.onMoneyAmountChanged(onChanged: (Double) -> Unit) {
@@ -120,8 +102,38 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun EditText.updateMoneyAmount(amount: Double, fromCurrency: Currency, toCurrency: Currency) {
+        var df = DecimalFormat("#.######")
         val rate = toCurrency.rate / fromCurrency.rate
-        this.setText((rate * amount).toString(), android.widget.TextView.BufferType.EDITABLE)
+        this.setText(df.format(rate * amount), android.widget.TextView.BufferType.EDITABLE)
     }
 
+    private fun updateViewsWhenCurrencyChanged() {
+        btnBaseCurrencySelector.text = mBaseCurrency.name
+        tvBaseCurrencySymbol.text = mBaseCurrency.symbol
+        tvBaseCurrencyUnit.updateCurrencyUnitInfo(mBaseCurrency, mQuoteCurrency)
+        etBaseCurrencyAmount.setText("")
+
+        btnQuoteCurrencySelector.text = mQuoteCurrency.name
+        tvQuoteCurrencySymbol.text = mQuoteCurrency.symbol
+        tvQuoteCurrencyUnit.updateCurrencyUnitInfo(mQuoteCurrency, mBaseCurrency)
+        etQuoteCurrencyAmount.setText("")
+
+        clearAmount()
+    }
+
+    private fun clearAmount() {
+        etBaseCurrencyAmount.setText("")
+        etQuoteCurrencyAmount.setText("")
+    }
+
+    private fun showCurrencySelectionDialog(checkedItem: Int, onItemSelected: (Int) -> Unit) {
+        val currencyNameList: Array<String> = mCurrencyData.currencyInfoList.toTypedArray()
+        AlertDialog.Builder(this@MainActivity)
+                .setTitle("Select currency")
+                .setSingleChoiceItems(currencyNameList, checkedItem, { dialog, which ->
+                    onItemSelected(which)
+                    dialog.dismiss()
+                })
+                .show()
+    }
 }
